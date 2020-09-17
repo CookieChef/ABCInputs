9eb2f1c31be2a8437a1efb633d7c7166bcb6ca97
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-16 22:41:23 -0400

    completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace ABCInputs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Well hello there...");
+            
+            var isRunning = true;
+
+            while(isRunning) {
+            Console.WriteLine("Please type: A,B or C");
+            Console.WriteLine("Type 'Q' if you would like to quit");
+
+            var letter = Console.ReadLine();
+
+            if (letter == "a" || letter == "b" || letter == "c" || letter == "A" || letter == "B" || letter == "C"){
+                Console.WriteLine("Good job!");
+            }else if (letter == "q" || letter == "Q") {
+                isRunning = false;
+            }
+            }
         }
     }
 }
